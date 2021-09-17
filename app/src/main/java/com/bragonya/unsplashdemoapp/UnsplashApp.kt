3f608bb97922e7d938e29764c8a1365bb32d93ab
bragonya <bragonya@gmail.com>
2021-09-17 01:49:04 -0600

    animated transition for navigation.

--- app/src/main/java/com/bragonya/unsplashdemoapp/UnsplashApp.kt
+++ app/src/main/java/com/bragonya/unsplashdemoapp/UnsplashApp.kt
@@ -1,5 +1,7 @@
 package com.bragonya.unsplashdemoapp
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,8 +14,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.bragonya.unsplashdemoapp.ui.SharedViewModel
@@ -21,17 +21,21 @@ import com.bragonya.unsplashdemoapp.ui.detail.DetailView
 import com.bragonya.unsplashdemoapp.ui.favorites.FavoritesView
 import com.bragonya.unsplashdemoapp.ui.home.HomeView
 import com.bragonya.unsplashdemoapp.ui.screens.Screen
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
 val items = listOf(
     Screen.Images,
     Screen.Favorites,
 )
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun UnsplashApp(sharedViewModel: SharedViewModel){
     MaterialTheme {
         val scaffoldState = rememberScaffoldState()
-        val navController = rememberNavController()
+        val navController = rememberAnimatedNavController()
         Scaffold(
             scaffoldState = scaffoldState,
             bottomBar = {
@@ -68,10 +72,16 @@ fun UnsplashApp(sharedViewModel: SharedViewModel){
             }
         ) { innerPadding ->
             val actions = remember(navController) { MainActions(navController) }
-            NavHost(navController, startDestination = Screen.Images.route, Modifier.padding(innerPadding) ) {
+            AnimatedNavHost(navController, startDestination = Screen.Images.route, Modifier.padding(innerPadding) ) {
                 composable(Screen.Images.route) { HomeView(sharedViewModel, navController) }
                 composable(Screen.Favorites.route) { FavoritesView(sharedViewModel, navController) }
-                composable(Screen.Detail.route) { DetailView(actions.upPress) }
+                composable(
+                    Screen.Detail.route,
+                    enterTransition = { _, _ -> slideInVertically(initialOffsetY = { it / 2 }, animationSpec = tween(500)) },
+                    exitTransition = { _, _ -> slideOutVertically(targetOffsetY = { it / 2 }, animationSpec = tween(500)) }
+                ) {
+                    DetailView(actions.upPress)
+                }
             }
         }
 
